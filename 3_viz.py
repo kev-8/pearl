3a79127d7f7336fcd1870bc4431c2bbee866ca88
kev <kev@email.com>
2022-07-15 09:01:24 -0400

    updates to viz

--- 3_viz.py
+++ 3_viz.py
@@ -1,6 +1,9 @@
 
 # load in libraries
-from dash import Dash, html, dcc
+from dash import Dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 
@@ -17,3 +20,34 @@ modeling_df = pd.read_csv('./modeling_df.csv')
 # topic modeling
 # global regional analysis 
 
+
+# ADD the landing page to the app
+
+
+app = Dash(__name__)
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'}
+
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
+
+fig = px.scatter(df, x="gdp per capita", y="life expectancy",
+                 size="population", color="continent", hover_name="country",
+                 log_x=True, size_max=60)
+
+fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
