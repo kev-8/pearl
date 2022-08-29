1fabe25d264a22d48f7c2a283dbec288b525ec20
kev <kev@email.com>
2022-08-28 22:36:20 -0400

    Added map and dropdown menus to app

--- pages/app_page.py
+++ pages/app_page.py
@@ -1,20 +1,34 @@
 import dash
-from dash import html
+import dash_bootstrap_components as dbc
+import dash_leaflet as dl
+from dash import html, dcc
 
 dash.register_page(__name__, path='/app', title='pearl', name='app_page')
 
 # TODO: 
-    # Add clickable world map
-    # Add country down menus
-    # Add timeframe dropdown menu
+    # use dbc to align the dropdown menus
+    # decrease map space -> top half 30%, bottom half 70%
+    # load in df and create filters based on menu selection
+
+url = 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
+attribution = '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> '
 
 colors = {'text': '#7FDBFF'}
 
-layout = html.Div(children=[
-    html.H1(children='placeholder',
-            style={
-                'textAlign': 'center',
-                'color': colors['text']
-            }
-    )
-])
+
+layout = html.Div([
+    dl.Map(dl.TileLayer(url=url, minZoom=1, maxZoom=4, attribution=attribution),
+           style={'width': '50%', 'height': '500px', 'float': 'left'}),
+    
+    html.Div(children=[
+        html.Label('Country'),
+        dcc.Dropdown(['Canada', 'France', 'United States'], 'Canada',
+                     placeholder='Select a country'),
+        html.Br(),
+        html.Label('Year'),
+        dcc.Dropdown(['2022', '2021', '2020'], '2022',
+                     placeholder='Select a year')
+        ],
+        style={'width': '25%', 'float': 'right'})
+    
+        ])
