b7aca5a640006d19ff539723e2c82938c0ce4913
kev <kev@email.com>
2022-10-29 17:50:21 -0400

    update to app page format

--- 3_viz.py
+++ 3_viz.py
@@ -2,12 +2,9 @@
 # load in libraries
 import dash
 from dash import Dash, html
-# import dash_bootstrap_components as dbc
+import dash_bootstrap_components as dbc
 # from dash.dependencies import Input, Output
 # import plotly.express as px
-import pandas as pd
-
-modeling_df = pd.read_csv('./modeling_df.csv')
 
 
 # Elements to include: 
@@ -22,10 +19,7 @@ modeling_df = pd.read_csv('./modeling_df.csv')
 
 
 
-app = Dash(__name__, use_pages=True)
-
-
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
+app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.CYBORG])
 
 
 app.layout = html.Div([
--- pages/app_page.py
+++ pages/app_page.py
@@ -2,33 +2,126 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_leaflet as dl
 from dash import html, dcc
+import pandas as pd
+import plotly.express as px
 
 dash.register_page(__name__, path='/app', title='pearl', name='app_page')
 
-# TODO: 
-    # use dbc to align the dropdown menus
-    # decrease map space -> top half 30%, bottom half 70%
-    # load in df and create filters based on menu selection
+# TODO: load in df and create filters based on menu selection
 
 url = 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
 attribution = '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> '
 
 colors = {'text': '#7FDBFF'}
 
+modeling_df = pd.read_csv('./modeling_df.csv')
+modeling_df['country'] = 'Canada'
+
+fig = px.bar(modeling_df, x="top_entity_names", y="country")
 
 layout = html.Div([
-    dl.Map(dl.TileLayer(url=url, minZoom=1, maxZoom=4, attribution=attribution),
-           style={'width': '50%', 'height': '500px', 'float': 'left'}),
     
-    html.Div(children=[
-        html.Label('Country'),
-        dcc.Dropdown(['Canada', 'France', 'United States'], 'Canada',
-                     placeholder='Select a country'),
+    # dbc.Row([
+    #     dbc.Col(
+    #         dbc.DropdownMenu(
+    #             label='Country',
+    #             menu_variant='dark',
+    #             toggle_style={'background': '#d4b1aa'},
+    #             children=[
+    #                 dbc.DropdownMenuItem('Canada'),
+    #                 dbc.DropdownMenuItem('United States'),
+    #                 dbc.DropdownMenuItem('France')
+    #             ]
+    #         ),
+            
+    #         dbc.DropdownMenu(
+    #             label='Year',
+    #             menu_variant='dark',
+    #             toggle_style={'background': '#d4b1aa'},
+    #             children=[
+    #                 dbc.DropdownMenuItem('2022'),
+    #                 dbc.DropdownMenuItem('2021'),
+    #                 dbc.DropdownMenuItem('2020')
+    #             ]
+    #         ),
+    #         align='center',
+    #         width=3
+    #     ),
+        
+    #     dbc.Col(
+    #         dl.Map(dl.TileLayer(url=url, minZoom=1, maxZoom=4, attribution=attribution)),
+    #         width=9
+    #         )
+    # ])
+    
+    dbc.Card(
+    dbc.CardBody([
+    dbc.Row([
+        dbc.Col(
+            [
+            html.Label('Country'),    
+            dcc.Dropdown(['Canada', 'France', 'United States'], 'Canada',
+                         placeholder='Select a country')
+            ]
+        ),
         html.Br(),
-        html.Label('Year'),
-        dcc.Dropdown(['2022', '2021', '2020'], '2022',
-                     placeholder='Select a year')
-        ],
-        style={'width': '25%', 'float': 'right'})
+        html.Br(),
+        dbc.Col(
+            [
+            html.Label('Year'),
+            dcc.Dropdown(['2022', '2021', '2020'], '2022',
+                         placeholder='Select a year')    
+            ]
+        ),
+        
+        # TODO: make map move based on country selection
+        # TODO: add border around the map
+        dl.Map(dl.TileLayer(url=url, minZoom=1, maxZoom=6, attribution=attribution),
+                style={'width': '55%', 'height': '300px', 'float': 'left'})
+    ])
+    ]),
+    color='#000'
+    ),
     
-        ])
+    dbc.Card(
+    dbc.CardBody([
+    dbc.Row([
+        dbc.Col(
+            [
+            html.Label('Entities'),
+            dcc.Graph(figure=fig)
+            ]
+        ),
+        dbc.Col(
+            [
+            html.Label('Topics'),
+            dcc.Graph(figure=fig)
+            ]
+        ),
+        dbc.Col(
+            [
+            html.Label('Sentiment'),
+            dcc.Graph(figure=fig)
+            ]
+        )
+    ])    
+    ]),
+    color='#000'    
+    )
+
+    
+    
+    # Dropdown menus using DCC instead of DBC. might need later for callbacks
+    
+    # html.Div(children=[
+    #     html.Label('Country'),
+    #     dcc.Dropdown(['Canada', 'France', 'United States'], 'Canada',
+    #                   placeholder='Select a country'),
+    #     html.Br(),
+    #     html.Label('Year'),
+    #     dcc.Dropdown(['2022', '2021', '2020'], '2022',
+    #                   placeholder='Select a year')
+    #     ],
+    #     style={'width': '25%', 'float': 'right'})
+    
+])
